deb57f0f61627bcc8608658650175af05af566be
kirtikumarpandya <kirti6988@gmail.com>
2020-02-12 14:22:31 +0100

    we can extract data from hdf5 file and return the list of 10 chunk numpy array.

--- improc/writeHDF5.py
+++ improc/writeHDF5.py
@@ -1,5 +1,32 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-def writeHDF5(path, image):
+def writeHDF5(path:str, image):
+
+    f = h5py.File(path, 'r')
+
+    keys =list(f.keys())
+
+    dset = f['data']
+
+    x = round(dset.shape[0] / 10)
+    y = round(dset.shape[1] / 10)
+
+    chunk1 = dset[:x, :y]
+    chunk2 = dset[x + 1:(2 * x), y + 1:(2 * y)]
+    chunk3 = dset[(2 * x) + 1:(3 * x), (2 * y) + 1:(3 * y)]
+    chunk4 = dset[(3 * x) + 1:(4 * x), (3 * y) + 1:(4 * y)]
+    chunk5 = dset[(4 * x) + 1:(5 * x), (4 * y) + 1:(5 * y)]
+    chunk6 = dset[(5 * x) + 1:(6 * x), (5 * y) + 1:(6 * y)]
+    chunk7 = dset[(6 * x) + 1:(7 * x), (6 * y) + 1:(7 * y)]
+    chunk8 = dset[(7 * x) + 1:(8 * x), (7 * y) + 1:(8 * y)]
+    chunk9 = dset[(8 * x) + 1:(9 * x), (8 * y) + 1:(9 * y)]
+    chunk10 = dset[(9 * x) + 1:, (9 * y) + 1:]
+
+
+
     """ creates a hdf5 file and writes the mask data
 
     Parameters
@@ -17,3 +44,5 @@ def writeHDF5(path, image):
     #       - create file
     #       - write data from the quadtree in image.mask in chunks to the file
     #       - maybe implement Error Messages
+
+    return [chunk1, chunk2, chunk3, chunk4, chunk5, chunk6, chunk7, chunk8, chunk9, chunk10]
