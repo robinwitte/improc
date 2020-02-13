3d73cb1c6a9c71d418645f709e57124fafa4b4ee
phbro101 <57494402+phbro101@users.noreply.github.com>
2020-02-13 15:19:46 +0100

    added test

--- Test.py
+++ Test.py
@@ -1,29 +1,11 @@
 import improc
 from improc.QuadTree import QuadTree
 import numpy as np
+import h5py
 
 
-path_load = 'data/hdf5_image/B01_0361_annotations_si_spacing64.hdf5'
+path_load = 'C:/Users/phili/PycharmProjects/untitled2/data/hdf5_image/B01_0361_annotations_si_spacing64.hdf5'
 image = improc.load(path_load, True)
 
-test = np.array([[500,250],[0,0],[3,25]])
-
-#array = np.ones((1000,1000))
-#print(array)
-
-
-#tree = QuadTree()
-
-#tree.store_data(array, 100, (0,0), (99,99))
-
-
-#path_load = 'data/json/B01_0361_annotations.json'
-#path_load = 'data/hdf5_image/B01_0361_annotations_si_spacing64.hdf5'
-
-
-#image = improc.load(path_load, True)
-
-
-#test = [(100,100,0), (120,143,5), (1000, 300, 200)]
-
-#print(image.check(test))
+test_file = np.load("", allow_pickle=True)["arr_0"]
+print(image.mask.data.check(test_file))
